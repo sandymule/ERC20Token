0836c1105cad8c2cf43eac76fe24f1476042c59c
Samuel Wong <sandymule@gmail.com>
2021-05-14 00:11:21 -0700

    constructor token sale

--- token_sale/migrations/2_deploy_contracts.js
+++ token_sale/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-const DappToken = artifacts.require("./DappToken.sol");
+var DappToken = artifacts.require("./DappToken.sol");
+var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(function() {
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  });
 };
